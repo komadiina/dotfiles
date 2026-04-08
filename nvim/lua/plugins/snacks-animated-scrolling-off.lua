ab839cfd21db4b012ccd38e50b179a7f02f914c4
okomadina <ognjen.komadina@vodafonezigo.com>
2026-04-08 10:19:27 +0200

    .

---
+++ nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = false, -- Disable scrolling animations
+    },
+    explorer = {
+      layout = { layout = { position = "right" } },
+      git_status = true,
+      git_untracked = true,
+    },
+  },
+}
